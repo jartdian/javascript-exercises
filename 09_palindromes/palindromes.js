eec1dd5ad6f9a93551bab4e7de35d979ec2f1c79
artdian <46709199+jartdian@users.noreply.github.com>
2023-01-28 20:58:53 +0100

    finish 01-09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arrFromStr = [...str];
+    let reversedArrFromStr = [...str].reverse()
+    return arrFromStr.toString().toLowerCase().replace(/[^a-zA-Z0-9]/g,"") ==
+    reversedArrFromStr.toString().toLowerCase().replace(/[^a-zA-Z0-9]/g,"")
 };
-
 // Do not edit below this line
 module.exports = palindromes;
